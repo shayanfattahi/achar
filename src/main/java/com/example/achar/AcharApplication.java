8abd029fa1a833ce4b4747a2b52ad018412471dc
shayan fattahi <shayanfattahiii@yahoo.com>
2023-02-10 20:18:55 +0330

    Initial commit

---
+++ src/main/java/com/example/achar/AcharApplication.java
@@ -0,0 +1,13 @@
+package com.example.achar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcharApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcharApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/achar/AcharApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.achar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcharApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
